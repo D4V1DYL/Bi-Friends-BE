eec052d92165b80258cdf78c9e558b2ed6373d45
Kradnuzz <grand_razor98@yahoo.com>
2025-02-23 09:34:21 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, HTTPException,Request
+from pydantic import BaseModel
+from middleware import log_requests
+from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
+from auth import router as auth_router
+from dashboard import router as dashboard_router
+from product import router as product_router
+from superadmin import router as superadmin_router
+
+app = FastAPI()
+
+limiter = Limiter(key_func=get_remote_address)
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
+
+
+# app.include_router(auth_router, prefix="/auth")
+# app.include_router(dashboard_router, prefix="/dashboard")
+
+
+app.middleware("https")(log_requests)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Health Check
+@app.get("/")
+@limiter.limit("50/minute")
+async def root(request: Request):
+    return {"message": "AI REST API is running"}
+
+
+
